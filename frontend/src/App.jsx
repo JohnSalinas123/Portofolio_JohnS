4dbc73c4a4db82c4975c9afd7d9b428216ed6761
JohnSalinas123 <salinasjohn257@gmail.com>
2023-12-18 13:16:34 -0800

    started responsive navbar, wip

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,20 @@
-import { useState } from 'react'
 import './App.css'
+import { Routes, Route} from 'react-router-dom'
+import { Navbar } from './components/Navbar'
+import { Home, Projects, About, Blog } from './components/pages'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-    </>
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/"  element={<Home />}/>
+        <Route path="/projects"  element={<Projects />}/>
+        <Route path="/about"  element={<About />}/>
+        <Route path="/blog"  element={<Blog />}/>
+      </Routes>
+    </div>
   )
 }
 

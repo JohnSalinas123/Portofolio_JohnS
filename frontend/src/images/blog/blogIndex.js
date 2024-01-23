45bb1f54cd990c119b811d1bbab0b716a35deaae
JohnSalinas123 <salinasjohn257@gmail.com>
2024-01-22 21:09:37 -0800

    add blog page image grid

---
+++ frontend/src/images/blog/blogIndex.js
@@ -0,0 +1,6 @@
+import lighthouse from "./lighthouse.png"
+import donuts from "./donuts.png"
+import dungeon from "./dungeon.png"
+
+
+export { lighthouse, donuts, dungeon }
--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -1,7 +1,45 @@
 import React from 'react'
+import '../styles/blog.css'
+import { donuts, lighthouse, dungeon} from "../images/blog/blogIndex.js"
+
+
+
 
 export const Blog = () => {
   return (
-    <div className="narrow-content-container">Blog</div>
+    <div className="narrow-content-container">
+      <ImageGallery />
+    </div>
   )
+}
+
+const ImageGallery = () => {
+
+  return (
+    <div className="image-grid">
+      <Image src={dungeon} alt="Blender Donut Tutorial" type={"wide"} />
+      <Image src={donuts} alt="Blender Donut Tutorial" type={"large"}/>
+      <Image src={donuts} alt="Blender Donut Tutorial"/>
+      <Image src={donuts} alt="Blender Donut Tutorial"/>
+      <Image src={lighthouse} alt="Blender Donut Tutorial" type={"wide"}/>
+    </div>
+  )
+
+}
+
+const Image = ({src,alt, type}) => {
+
+  var classNames = ""
+  if (type == 'wide') {
+    classNames = "image-grid-col-2"
+  } else if (type == 'large') {
+    classNames = "image-grid-col-2 image-grid-row-2"
+  }
+
+  return (
+    <div className={classNames}>
+      <img src={src} alt={alt} />
+    </div>
+  )
+
 }

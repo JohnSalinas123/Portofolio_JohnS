c26389f2b7b166c4791decdc89902faa24e7b25d
JohnSalinas123 <salinasjohn257@gmail.com>
2024-01-24 21:36:37 -0800

    add blog images overlay info

--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import './Blog.css'
 import { donuts, lighthouse, dungeon} from "../images/blog/blogIndex.js"
+import { CSSTransition } from 'react-transition-group';
 
 
 
 
 export const Blog = () => {
   return (
-    <div className="narrow-content-container">
-      <ImageGallery />
-    </div>
+    <CSSTransition in={true} appear={true} timeout={1000} classNames="fade">
+      <div className="narrow-content-container blog-content">
+        <ImageGallery />
+      </div>
+    </CSSTransition>
   )
 }
 
@@ -17,30 +20,31 @@ const ImageGallery = () => {
 
   return (
     <div className="image-grid">
-      <Image src={dungeon} alt="Blender Donut Tutorial" type={"wide"} />
-      <Image src={donuts} alt="Blender Donut Tutorial" type={"large"}/>
+      <Image src={dungeon} alt="Blender Donut Tutorial" type={"wide"} overlayTitle={"Blender Donut Tutorial"} />
+      <Image src={donuts} alt="Blender Donut Tutorial" type={"large"} overlayTitle={"Blender Donut Tutorial"}/>
+      <Image src={donuts} alt="Blender Donut Tutorial" overlayTitle={"Blender Donut Tutorial"}/>
       <Image src={donuts} alt="Blender Donut Tutorial"/>
-      <Image src={donuts} alt="Blender Donut Tutorial"/>
-      <Image src={lighthouse} alt="Blender Donut Tutorial" type={"wide"}/>
-      <Image src={donuts} alt="Blender Donut Tutorial"/>
-      <Image src={donuts} alt="Blender Donut Tutorial"/>
+      
     </div>
   )
 
 }
 
-const Image = ({src,alt, type}) => {
+const Image = ({src,alt, type, overlayTitle}) => {
 
-  var classNames = ""
+  var classNames = "image-container"
   if (type == 'wide') {
-    classNames = "image-grid-col-2"
+    classNames = "image-container image-grid-col-2"
   } else if (type == 'large') {
-    classNames = "image-grid-col-2 image-grid-row-2"
+    classNames = "image-container image-grid-col-2 image-grid-row-2"
   }
 
   return (
     <div className={classNames}>
       <img src={src} alt={alt} />
+      <div className="image-overlay">
+        {overlayTitle}
+      </div>
     </div>
   )
 

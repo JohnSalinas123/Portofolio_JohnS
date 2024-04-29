9d2a9eb4d8cc092da66f473bb5caf6569cbd16a3
JohnSalinas123 <salinasjohn257@gmail.com>
2024-04-28 20:02:51 -0700

    add responsiveness to blog page

--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -9,9 +9,7 @@ import './Blog.css'
 export const Blog = () => {
   return (
     <CSSTransition in={true} appear={true} timeout={1000} classNames="fade">
-      <div className="narrow-content-container">
         <ImageGallery />
-      </div>
     </CSSTransition>
   )
 }
@@ -19,20 +17,25 @@ export const Blog = () => {
 const ImageGallery = () => {
 
   return (
-    <ResponsiveMasonry
-      columnsCountBreakPoints={{350: 1, 750: 2, 900: 3}}
-  >
-      <Masonry gutter="16px">
-          <Image src={dungeon} alt="Blender Donut Tutorial"  overlayTitle={"Blender Donut Tutorial"} />
-          <Image src={donuts} alt="Blender Donut Tutorial"  overlayTitle={"Blender Donut Tutorial"}/>
-          <Image src={lighthouse} alt="Blender Donut Tutorial" overlayTitle={"Blender Donut Tutorial"}/>
-          <Image src={donuts} alt="Blender Donut Tutorial"  overlayTitle={"Blender Donut Tutorial"}/>
-          <Image src={donuts} alt="Blender Donut Tutorial" overlayTitle={"Blender Donut Tutorial"}/>
-          <Image src={donuts} alt="Blender Donut Tutorial" overlayTitle={"Blender Donut Tutorial"}/>
-          <Image src={donuts} alt="Blender Donut Tutorial" overlayTitle={"Blender Donut Tutorial"}/>
-          <Image src={donuts} alt="Blender Donut Tutorial" overlayTitle={"Blender Donut Tutorial"}/>
-      </Masonry>
-    </ResponsiveMasonry>
+    <div id="blog-masonry-box">
+      <div id="blog-inner">
+          <ResponsiveMasonry
+              columnsCountBreakPoints={{350: 1, 780: 2, 1500: 3}}
+          >
+              <Masonry gutter="16px">
+                  <Image src={dungeon} alt="Blender Donut Tutorial"  overlayTitle={"Blender Donut Tutorial"} />
+                  <Image src={donuts} alt="Blender Donut Tutorial"  overlayTitle={"Blender Donut Tutorial"}/>
+                  <Image src={lighthouse} alt="Blender Donut Tutorial" overlayTitle={"Blender Donut Tutorial"}/>
+                  <Image src={donuts} alt="Blender Donut Tutorial"  overlayTitle={"Blender Donut Tutorial"}/>
+                  <Image src={donuts} alt="Blender Donut Tutorial" overlayTitle={"Blender Donut Tutorial"}/>
+                  <Image src={donuts} alt="Blender Donut Tutorial" overlayTitle={"Blender Donut Tutorial"}/>
+                  <Image src={donuts} alt="Blender Donut Tutorial" overlayTitle={"Blender Donut Tutorial"}/>
+                  <Image src={donuts} alt="Blender Donut Tutorial" overlayTitle={"Blender Donut Tutorial"}/>
+              </Masonry>
+            </ResponsiveMasonry>
+      </div>
+    </div>
+    
     
 
   )

72d7910cac279eb1896ffd3d1b42a9e46cfd1625
JohnSalinas123 <salinasjohn257@gmail.com>
2024-04-28 20:07:42 -0700

    add better styling for project cards, responsiveness

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import { donuts, lighthouse, dungeon} from "../images/blog/blogIndex.js"
+import { mybookshelf, polylang  } from '../images/project/projectIndex.js';
 import { CSSTransition } from 'react-transition-group';
 import './Projects.css'
 
 export const Projects = () => {
   return (
     <CSSTransition in={true} appear={true} timeout={1000} classNames="fade">
-      <div className="narrow-content-container">
         <ProjectGallery />
-      </div>
     </CSSTransition>
   )
 }
@@ -16,12 +15,18 @@ export const Projects = () => {
 const ProjectGallery = () => {
 
   return (
-    <div className="project-grid-box">
+    <div id="project-grid-box">
       <div className="project-grid">
-        <Project src={donuts} title={"Blender Donut"} short_descrip={"3D Model of a plate of donuts"} date={"11/23/2023"} alt={"A 3D model of a plate with a few donuts on it"} links={{"Github": "https://github.com/JohnSalinas123/simpleCalculator"}} />
-        <Project src={donuts} title={"Blender Donut"} short_descrip={"3D Model of a plate of donuts"} date={"11/23/2023"} alt={"A 3D model of a plate with a few donuts on it"} links={{"Github": "https://github.com/JohnSalinas123/simpleCalculator"}}/>
-        <Project src={donuts} title={"Blender Donut"} short_descrip={"3D Model of a plate of donuts"} date={"11/23/2023"} alt={"A 3D model of a plate with a few donuts on it"} links={{"Github": "https://github.com/JohnSalinas123/simpleCalculator"}}/>
-        <Project src={donuts} title={"Blender Donut"} short_descrip={"3D Model of a plate of donuts"} date={"11/23/2023"} alt={"A 3D model of a plate with a few donuts on it"} links={{"Github": "https://github.com/JohnSalinas123/simpleCalculator"}}/>
+        <Project src={polylang} title={"CPP EventMap"} short_descrip={"An event map to help students find events"} date={"11/23/2023"} alt={"An image of a web application using a map to display event locations on e CPP campus using markers tsting if the text overflow works,ecececefewfwegwgwgwgergergergeg"} links={{"Github": "https://github.com/JohnSalinas123/simpleCalculator"}} />   
+        <Project src={mybookshelf} title={"Blender Donut"} short_descrip={"3D Model of a plate of donuts"} date={"11/23/2023"} alt={"A  model of a plate with a few donuts on it"} links={{"Github": "https://github.com/JohnSalinas123/simpleCalculator"}}/>
+        <Project src={mybookshelf} title={"Blender Donut"} short_descrip={"3D Model of a plate of donuts"} date={"11/23/2023"} alt={"A 3D model of a plate with a few donuts on it"} links={{"Github": "https://github.com/JohnSalinas123/simpleCalculator"}}/>
+        <Project src={polylang} title={"CPP EventMap"} short_descrip={"An event map to help students find events on campus"} date={"11/23/2023"} alt={"An image of a web application using a map to display event locations on the CPP campus using markers"} links={{"Github": "https://github.com/JohnSalinas123/simpleCalculator"}} />   
+        <Project src={mybookshelf} title={"Blender Donut"} short_descrip={"3D Model of a plate of donuts"} date={"11/23/2023"} alt={"A 3D model of a plate with a few donuts on it"} links={{"Github": "https://github.com/JohnSalinas123/simpleCalculator"}}/>
+        <Project src={mybookshelf} title={"Blender Donut"} short_descrip={"3D Model of a plate of donuts"} date={"11/23/2023"} alt={"A 3D model of a plate with a few donuts on it"} links={{"Github": "https://github.com/JohnSalinas123/simpleCalculator"}}/>
+
+        
+        
+        
       </div>
     </div>
     
@@ -36,7 +41,10 @@ const Project = ({src, title, short_descrip,date, alt, links}) => {
   return (
     <div className="project-outer">
       <div className="project-inner">
-        <img src={src} alt={alt} />
+        <div className="project-image">
+          <img src={src} alt={alt} />
+        </div>
+        
         <div className="project-footer">
           <div className="project-links inter-400">
             {links && Object.keys(links).map((linkName) => (
@@ -49,7 +57,7 @@ const Project = ({src, title, short_descrip,date, alt, links}) => {
                 /* <p className="project-date">{date}</p>*/
               }
 
-              <p className="project-short-descrip inter-300">{short_descrip}</p>
+              <p className="project-short-descrip inter-400">{short_descrip}</p>
           </div>
           
         </div>

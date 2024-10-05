a31806a5cefa1bf79d3cb1415f1a378cf0b0b963
JohnSalinas123 <salinasjohn257@gmail.com>
2024-10-04 17:55:38 -0700

    update home page and general css

--- frontend/src/sections/Home.jsx
+++ frontend/src/sections/Home.jsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import { DeskThreeDModel } from '../components/DeskThreeDModel'
-
 import './Home.css'
 
 export const Home = () => {
   return (
       <>
         <div id="home-box">
-          <div className="three-d-model-box">
-            <DeskThreeDModel />
+          <div id="intro-box">
+            <img id="intro-image" src="johnsalinas_profilepic.jpg" alt="Image of john salinas"/>
+            <div id="intro-text-box inter-400">
+                <p id="intro-text"> Hi!, I&apos;m <br/> John Salinas,<br/>Software Engineer  </p>
+                <p id="intro-sub-text">Dedicated to growth and development.</p>
+                <p id="intro-summary">Thank you for visiting! Take a look at my projects and experiences <br/>to learn more about my journey in software engineering.</p>
+            </div>
           </div>
-          <div className="intro-box inter-400">
-              <p> Hi, I'm John Salinas</p>
-          </div>
-
+          
         </div>
       </>  
     

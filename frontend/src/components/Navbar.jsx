f3a831a7aeab6e19380e96b5730e2914b316c047
JohnSalinas123 <salinasjohn257@gmail.com>
2024-10-07 17:45:55 -0700

    restyled, added links to route pages

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,71 +1,98 @@
-import {useState } from 'react'
+import { useState } from "react";
 import { FaLinkedin, FaGithub } from "react-icons/fa";
 import { IoIosMail } from "react-icons/io";
 import { RxHamburgerMenu } from "react-icons/rx";
-import { Link } from 'react-scroll'
-import "./Navbar.css"
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 export const Navbar = () => {
-    const [menuVisible, setMenuVisible] = useState(false)
+	const [menuVisible, setMenuVisible] = useState(false);
 
-    
+	return (
+		<nav>
+			<div
+				className="menu"
+				onClick={() => {
+					setMenuVisible(!menuVisible);
+				}}
+			>
+				<RxHamburgerMenu />
+			</div>
+			<div className="nav-edge-container logo-container">
+				<div className="logo-box">
+					<h1>
+						<Link to="/" className="inter-700 logo-link">
+							John Salinas
+						</Link>
+					</h1>
+				</div>
+			</div>
+			<div className="nav-container inter-500">
+				<ul className={menuVisible ? "open" : ""}>
+					<li>
+						<Link to="/" className="nav-element">
+							Home
+						</Link>
+					</li>
+					<li>
+						<Link to="/projects" className="nav-element">
+							Projects
+						</Link>
+					</li>
+					<li>
+						<Link to="/progress" className="nav-element">
+							Progress
+						</Link>
+					</li>
 
-    return (
-        <nav >
-            <div className="menu" onClick={() => {setMenuVisible(!menuVisible)}}>
-                <RxHamburgerMenu />
-            </div>
-            <div className="nav-edge-container logo-container">
-                <div className="logo-box">
-                    <h1>
-                        <Link 
-                        to="top"
-                        spy={true}
-                        smooth={true}
-                        duration={500}
-                        className="inter-700 logo-link"
-                    >John Salinas</Link>
-                    </h1>
-                    
-                </div>
-                
-            </div>
-            <div className="nav-container inter-500">
-                <ul className={menuVisible ? "open" : ""}>
-                    <li>
-                        <Link 
-                            to="aboutme"
-                            spy={true}
-                            smooth={true}
-                            duration={500}
-                            offset={-80}
-                            className="nav-element"
-                        >About</Link>
-                    </li>
-                    <li>
-                        <Link 
-                            to="projects"
-                            spy={true}
-                            smooth={true}
-                            duration={500}
-                            offset={-80}
-                            className="nav-element"
-                        >Projects</Link>
-                    </li>
-                    
-                    <li className="menu-socials">
-                        <a href="https://www.linkedin.com/in/johnsalinas123/" target="_blank" rel="noreferrer"><FaLinkedin className="social-icon"/></a>
-                        <a href = "mailto: salinasjohn257@gmail.com" target="_blank" rel="noreferrer"><IoIosMail className="social-icon"/></a>
-                        <a href = "https://github.com/JohnSalinas123" target="_blank" rel="noreferrer"><FaGithub className="social-icon"/></a>
-                    </li>
-                </ul>
-            </div>
-            <div className="nav-edge-container social-container">
-                <a href="https://www.linkedin.com/in/johnsalinas123/" target="_blank" rel="noreferrer"><FaLinkedin className="social-icon"/></a>
-                <a href = "mailto: salinasjohn257@gmail.com" target="_blank" rel="noreferrer"><IoIosMail className="social-icon"/></a>
-                <a href = "https://github.com/JohnSalinas123" target="_blank" rel="noreferrer"><FaGithub className="social-icon"/></a>
-            </div>
-            
-        </nav>
-    )
-}
+					<li className="menu-socials">
+						<a
+							href="https://www.linkedin.com/in/johnsalinas123/"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<FaLinkedin className="social-icon" />
+						</a>
+						<a
+							href="mailto: salinasjohn257@gmail.com"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<IoIosMail className="social-icon" />
+						</a>
+						<a
+							href="https://github.com/JohnSalinas123"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<FaGithub className="social-icon" />
+						</a>
+					</li>
+				</ul>
+			</div>
+			<div className="nav-edge-container social-container">
+				<a
+					href="https://www.linkedin.com/in/johnsalinas123/"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<FaLinkedin className="social-icon" />
+				</a>
+				<a
+					href="mailto: salinasjohn257@gmail.com"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<IoIosMail className="social-icon" />
+				</a>
+				<a
+					href="https://github.com/JohnSalinas123"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<FaGithub className="social-icon" />
+				</a>
+			</div>
+		</nav>
+	);
+};

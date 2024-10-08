ce403e873866c1356ae985f7cf3c9b6918a05553
JohnSalinas123 <salinasjohn257@gmail.com>
2024-10-07 17:45:35 -0700

    add react router dom and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,21 @@
-import { Navbar } from './components/Navbar'
-import { Home, Projects } from './sections/sectionsIndex.js'
-import './App.scss'
-import { AboutMe } from './sections/AboutMe.jsx'
-
-
+import { Navbar } from "./components/Navbar";
+import { Home, Projects } from "./sections/sectionsIndex.js";
+import "./App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-
-  return (
-    <>
-      <div id="top"></div>
-      <Navbar />
-      <div id="main-container">
-        <Home />
-        <AboutMe />
-        <Projects />
-      </div>
-      
-    </>  
-      
-    
-  )
+	return (
+		<>
+			<div id="top"></div>
+			<Navbar />
+			<div id="main-container">
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/projects" element={<Projects />} />
+					</Routes>
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;

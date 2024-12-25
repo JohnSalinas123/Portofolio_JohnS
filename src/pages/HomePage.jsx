4e2e5e56b4b334fc6376c3635d6919192801230b
JohnSalinas123 <salinasjohn257@gmail.com>
2024-12-24 17:41:02 -0800

    changed homepage into scrolling format, minor change to scrollbar colors

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,34 +1,13 @@
 import "./HomePage.css";
 import { CSSTransition } from "react-transition-group";
+import { ProjectsSection } from "../sections/ProjectsSection.jsx";
+import { StudyLogSection } from "../sections/StudyLogSection.jsx";
 
 export const HomePage = () => {
 	return (
 		<>
-			<CSSTransition in={true} appear={true} timeout={1000} classNames="fade">
-				<div id="home-box">
-					<div id="intro-box">
-						<img
-							id="intro-image"
-							src="johnsalinas_profilepic.jpg"
-							alt="Image of john salinas"
-						/>
-						<div id="intro-text-box inter-400">
-							<p id="intro-text">
-								{" "}
-								Hi!, I&apos;m <br /> John Salinas,
-								<br />
-								Software Engineer{" "}
-							</p>
-							<p id="intro-sub-text">Dedicated to growth and development.</p>
-							<p id="intro-summary">
-								Thank you for visiting! Take a look at my projects and
-								experiences <br />
-								to learn more about my journey in software engineering.
-							</p>
-						</div>
-					</div>
-				</div>
-			</CSSTransition>
+			<ProjectsSection />
+			<StudyLogSection />
 		</>
 	);
 };

0039f313b5ff017a37188fc1d6b5677df60cf8c8
JohnSalinas123 <salinasjohn257@gmail.com>
2024-12-25 12:15:21 -0800

    updated app to use mantine appshell

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,103 @@
-import { Navbar } from "./components/Navbar";
 import { HomePage } from "./pages/HomePage.jsx";
 import "./App.scss";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import { useDisclosure } from "@mantine/hooks";
+
+import { UnstyledButton, Title } from "@mantine/core";
+
+import { AppShell, Burger, Group } from "@mantine/core";
+import { Link as ScrollLink } from "react-scroll";
+import { Link as RouterLink } from "react-router-dom";
+import { AboutMePage } from "./pages/AboutMePage.jsx";
+
+import RootLayout from "./layouts/RootLayout.jsx";
 
 function App() {
+	const [opened, { toggle }] = useDisclosure();
+
 	return (
 		<>
-			<div id="top"></div>
-			<Navbar />
-			<div id="main-container">
-				<Routes>
-					<Route path="/" element={<HomePage />} />
-				</Routes>
-			</div>
+			<AppShell
+				header={{ height: 60 }}
+				navbar={{
+					width: 300,
+					breakpoint: "sm",
+					collapsed: { desktop: true, mobile: !opened },
+				}}
+				padding="md"
+			>
+				<AppShell.Header>
+					<Group h="100%" px="md">
+						<Burger
+							opened={opened}
+							onClick={toggle}
+							hiddenFrom="sm"
+							size="sm"
+						/>
+						<Group justify="space-between" style={{ flex: 1 }}>
+							<Title order={1} size="h2" className="logo-box">
+								<RouterLink
+									to="/"
+									smooth="true"
+									duration={500}
+									className="inter-700 logo-link"
+								>
+									John Salinas
+								</RouterLink>
+							</Title>
+							<Group ml="xl" gap={20} visibleFrom="sm">
+								<ScrollLink
+									to="top"
+									smooth="true"
+									duration={500}
+									className="inter-700 logo-link"
+								>
+									<UnstyledButton>Home</UnstyledButton>
+								</ScrollLink>
+								<ScrollLink
+									to="projects"
+									smooth="true"
+									duration={500}
+									className="inter-700 logo-link"
+								>
+									<UnstyledButton>Projects</UnstyledButton>
+								</ScrollLink>
+								<ScrollLink
+									to="skills"
+									smooth="true"
+									duration={500}
+									className="inter-700 logo-link"
+								>
+									<UnstyledButton>Skills</UnstyledButton>
+								</ScrollLink>
+								<RouterLink
+									to="/log"
+									duration={500}
+									className="inter-700 logo-link"
+								>
+									<UnstyledButton>Log</UnstyledButton>
+								</RouterLink>
+							</Group>
+						</Group>
+					</Group>
+				</AppShell.Header>
+
+				<AppShell.Navbar py="md" px={4}>
+					<UnstyledButton>Home</UnstyledButton>
+					<UnstyledButton>Blog</UnstyledButton>
+					<UnstyledButton>Contacts</UnstyledButton>
+					<UnstyledButton>Support</UnstyledButton>
+				</AppShell.Navbar>
+
+				<AppShell.Main>
+					<Routes>
+						<Route path="/" element={<RootLayout />}>
+							<Route index element={<HomePage />} />
+							<Route path="/log" element={<AboutMePage />} />
+						</Route>
+					</Routes>
+				</AppShell.Main>
+			</AppShell>
 		</>
 	);
 }

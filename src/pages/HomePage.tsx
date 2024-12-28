74028465c5675ac145071227cbc8dbc3bc6a9aeb
JohnSalinas123 <salinasjohn257@gmail.com>
2024-12-28 09:59:32 -0800

    updated homepage styling and sections

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,14 +3,19 @@ import { ProjectsSection } from "../sections/ProjectsSection";
 import { StudyLogSection } from "../sections/StudyLogSection";
 
 import { Stack } from "@mantine/core";
+import { IntroSection } from "../sections/IntroSection";
+import { TechSkillsSection } from "../sections/TechSkillsSection";
 
 export const HomePage = () => {
 	return (
 		<>
-			<Stack justify="center" align="center">
+			<Stack gap={0} justify="center" align="center">
 				<div id="top"></div>
-				<ProjectsSection />
-				<StudyLogSection />
+				<IntroSection />
+				<div className="homepage-content">
+					<ProjectsSection />
+					<TechSkillsSection />
+				</div>
 			</Stack>
 		</>
 	);

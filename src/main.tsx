9eb968cebdecc7fc64917a0a9eccf674b4f2dd79
JohnSalinas123 <salinasjohn257@gmail.com>
2024-12-28 10:00:49 -0800

    add Montserrat, poppins, opensans fonts, add mantine theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,19 @@
-import React from "react";
-import ReactDOM, { createRoot } from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
-import { MantineProvider } from "@mantine/core";
+import { createTheme, MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
+
+const theme = createTheme({
+	fontFamily: "Open Sans",
+	headings: { fontFamily: "Montserrat" },
+});
 
 createRoot(document.getElementById("root")!).render(
 	<BrowserRouter>
-		<MantineProvider>
+		<MantineProvider theme={theme}>
 			<App />
 		</MantineProvider>
 	</BrowserRouter>

90c55168bf9123b0ee87b9e0d1898e99451a2e6d
JohnSalinas123 <salinasjohn257@gmail.com>
2025-06-05 12:11:00 -0700

    updated general container-center styling to work with new navbar/page/content setup

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,12 +1,18 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "../components/Navbar";
+import { Space } from "@mantine/core";
 
 const RootLayout = () => {
 	return (
 		<>
 			<main>
 				<Navbar />
-				<Outlet />
+				<div className="container-center">
+					<div className="content-container">
+						<Outlet />
+						<Space h="25px" />
+					</div>
+				</div>
 			</main>
 		</>
 	);

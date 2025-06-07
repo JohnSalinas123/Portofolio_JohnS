ccd8a1cbcd11a5d214aea834a11171759ccc85d8
JohnSalinas123 <salinasjohn257@gmail.com>
2025-06-07 13:23:00 -0700

    fixed study page layout styling

--- src/pages/StudyLogPage/StudyPage.tsx
+++ src/pages/StudyLogPage/StudyPage.tsx
@@ -1,10 +1,10 @@
 import "./StudyPage.css";
 
 import {
-	Spoiler,
 	Text,
 	Group,
 	useMantineTheme,
+	Paper,
 } from "@mantine/core";
 
 import studyData from "../../data/studylog_data.json";
@@ -18,31 +18,34 @@ export function StudyPage() {
 			<div className="study-outer">
 				{studyData.map((item, index) => (
 					<>
-						<div className="study-node" key={index}>
-							<Group>
-								<span role="img" aria-label={"placeholder label"}>
-									{item.symbol}
-								</span>
-								<Text
-									className="study-node-title" c={theme.white}
-								>
-									{item.title}
-								</Text>
-							</Group>
-							<Group>
-								{item.categories.map(category => (
-									<Text className="study-category">[ {category} ]</Text>
-								))}
-							</Group>
-							<div className="study-node-text">
-								{item.description &&
-								item.description
-									.split("\\n")
-									.map((line, i) => <Text key={i}>{line}</Text>)
-								}
-							</div>
-							
-						</div>
+						<Group align="flex-start" justify="space-between" wrap="nowrap" gap={30}>
+							<Text className="study-time">{`${item.start} - ${item.end}`}</Text>
+							<Paper withBorder className="study-node" key={index}>
+								<Group>
+									<span role="img" aria-label={"placeholder label"}>
+										{item.symbol}
+									</span>
+									<Text
+										className="study-node-title" c={theme.white}
+									>
+										{item.title}
+									</Text>
+								</Group>
+								<Group>
+									{item.categories.map(category => (
+										<Text className="study-category">[ {category} ]</Text>
+									))}
+								</Group>
+								<div className="study-node-text">
+									{item.description &&
+									item.description
+										.split("\\n")
+										.map((line, i) => <Text key={i}>{line}</Text>)
+									}
+								</div>
+								
+							</Paper>
+						</Group>
 					</>
 				))
 
